dc2706489d258c4ec9351f4d013e952b9023c1a1
clone <bjbs08@qq.com>
2020-12-16 13:17:09 +0800

    侧边菜单栏(未添加无限级分类)

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,57 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // import Home from '../views/Home.vue'
 import Layout from '../views/layout/Index.vue'
-
+// import Home from '../views/Home.vue'
+import Role from '../views/manage/Role.vue'
+import User from '../views/manage/User.vue'
 const routes = [
   {
     path: '/layout',
     name: 'layout',
+    hidden: true,
+    meta: {
+      title: "布局"
+    },
     component: Layout
   },
   {
     path: '/about',
     name: 'About',
+    hidden: true,
+    meta: {
+      title: "关于"
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/manage',
+    name: 'manage',
+    meta: {
+      title: "管理总台"
+    },
+    component: Layout,
+    children: [
+      {
+        path: '/user',
+        name: 'user',
+        meta: {
+          title: "用户管理"
+        },
+        component: User,
+      },
+      {
+        path: '/role',
+        name: 'role',
+        meta: {
+          title: "角色管理"
+        },
+        component: Role,
+      }
+    ]
+  },
 ]
 
 const router = createRouter({
--- src/views/layout/components/Aside.vue
+++ src/views/layout/components/Aside.vue
@@ -1,13 +1,45 @@
 <template>
- <div>
-  侧边栏
- </div>
+   <a-menu
+      v-model:openKeys="openKeys"
+      v-model:selectedKeys="selectedKeys"
+      mode="inline"
+      theme="dark"
+      :inline-collapsed="collapsed"
+    >
+      <template v-for="item in router" >
+          <template v-if="!item.hidden">
+            <a-menu-item v-if="!item.children" :key="item.path">
+              <span>{{item.meta && item.meta.title }}</span>
+            </a-menu-item>
+            <a-sub-menu v-else :key="item.path">
+              <template #title>
+                <span>{{item.meta && item.meta.title}}</span>
+              </template>
+              <a-menu-item v-for="child in item.children" :key="child.path">{{child.meta && child.meta.title}}</a-menu-item>
+            </a-sub-menu>
+          </template>
+      </template>
+    </a-menu>
 </template>
 
 <script>
- export default {
-
- }
+import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
+export default {
+  name: "Aside",
+  setup() {
+    const { options } = useRouter()
+    const router = options.routes
+    const data = reactive({
+      selectedKeys: ['1'],
+      openKeys: ['sub1'],
+    })
+    return {
+      data,
+      router
+    }
+  }
+}
 </script>
 
 <style scoped>
